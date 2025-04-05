39fed47a8f9fb163b41c79a03ce3fabf25c1e204
DELL <DELL@DESKTOP-LH71MIK>
2025-04-05 23:09:26 +0530

    Add project files.

---
+++ GitPracticalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitPracticalProject.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
